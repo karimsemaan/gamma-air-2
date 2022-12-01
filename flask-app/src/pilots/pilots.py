b63d661026eada8bdc4c118f000e3f90f1bb9b53
shreyathal <thalvayapati.s@northeastern.edu>
2022-12-01 12:44:58 -0500

    Update pilots.py

--- flask-app/src/pilots/pilots.py
+++ flask-app/src/pilots/pilots.py
@@ -8,3 +8,12 @@ pilots = Blueprint('pilots', __name__)
 def home():
     return '<h1>Pilots: Home</h1>'
 
+# SCHEDULES
+
+# Display's the pilot's current schedule
+
+# allows the pilot to submit rescheduling requests
+
+# REVIEWS
+
+# allows the pilot to view reviews of the flights they have been responsible for
