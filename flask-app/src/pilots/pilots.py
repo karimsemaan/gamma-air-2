ca8a833a729975ae3ce8b076c049de10dfe29549
shreyathal <thalvayapati.s@northeastern.edu>
2022-12-01 12:56:02 -0500

    my first route :)

--- flask-app/src/pilots/pilots.py
+++ flask-app/src/pilots/pilots.py
@@ -4,6 +4,7 @@ from src import db
 
 pilots = Blueprint('pilots', __name__)
 
+
 @pilots.route('/home', methods=['GET'])
 def home():
     return '<h1>Pilots: Home</h1>'
@@ -14,6 +15,13 @@ def home():
 
 # allows the pilot to submit rescheduling requests
 
+
 # REVIEWS
 
-# allows the pilot to view reviews of the flights they have been responsible for
+
+# displays all the reviews of all the flights
+# this pilot has been responsible for
+@pilots.route('/my-reviews', methods=['GET'])
+def pilot_reviews():
+    return '<h1>Pilots: My Reviews<h1>'
+
