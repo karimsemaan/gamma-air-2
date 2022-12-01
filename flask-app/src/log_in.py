81c9008b0c5e13e8af3170bc406a01597659e544
Shai Mann-Robison <mann.robison@gmail.com>
2022-12-01 13:22:35 -0500

    Update log_in.py

--- flask-app/src/log_in.py
+++ flask-app/src/log_in.py
@@ -4,16 +4,10 @@ log_in = Blueprint('log_in', __name__)
 
 
 # this is the home route.
-# it redirects to the main log in route
+# has three buttons to redirect to the right login paths
 @log_in.route('/', methods=['GET'])
 def home():
     return '<h1>Home</h1>'
-
-
-# main log in route (contains three buttons to redirect to login types)
-@log_in.route('/log_in', methods=['GET'])
-def log_in_main():
-    return '<h1>Login Main</h1>'
 
 
 # takes log in type as part of url (pilots/customers/representatives)
