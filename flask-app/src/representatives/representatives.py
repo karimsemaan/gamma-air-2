fe2b1e1bebd5255d3b4402e0ae5c41031cd5844e
Shai Mann-Robison <mann.robison@gmail.com>
2022-11-30 21:18:03 -0500

    fix capitalization cuz oops

--- flask-app/src/representatives/representatives.py
+++ flask-app/src/representatives/representatives.py
@@ -6,7 +6,7 @@ from src import db
 reps = Blueprint('reps', __name__)
 
 
-# contains buttons to redirect to all main (top level) pages for representatives
+# Contains buttons to redirect to all main (top level) pages for representatives
 # the ones I can think of right now:
 # 1) /matching-pilots-flights
 # 2) /answer-questions
@@ -18,9 +18,9 @@ def home():
 # MATCHING FLIGHTS AND PILOTS #
 
 
-# displays a view allowing the user to select a pilot and a flight
+# Displays a view allowing the user to select a pilot and a flight
 # assigning that pilot to that flight.
-# this means getting all flights that are unassigned, and then all pilots.
+# This means getting all flights that are unassigned, and then all pilots.
 # A pilot can be assigned to either pilot or copilot a flight.
 # When this is done, it hits the endpoint in this file at '/matching' with the appropriate query parameters
 @reps.route('/matching-pilots-flights', methods=['GET'])
@@ -28,7 +28,7 @@ def assign_pilots():
     return '<h1>Representatives: Assign Pilots to Flights</h1>'
 
 
-# this endpoint might break right now, since it is a POST but has code inside it as if it were a GET (returning)
+# This endpoint might break right now, since it is a POST but has code inside it as if it were a GET (returning)
 #
 # This endpoint is responsible for assigning a pilot to a flight. It has three parameters:
 # 1) the pilot ID (the pilot to assign to the given flight)
@@ -51,7 +51,7 @@ def answer_questions():
     return '<h1>Representatives: View asked questions</h1>'
 
 
-# might not work also since POST
+# Might not work also since POST
 # Submits an answer to the database and redirects back to the '/answer-questions' route.
 @reps.route('/submit-answer/<questionID>?response=<responseText>', methods=['POST'])
 def assign_pilots(questionID, responseText):
