cbe2d458d3064c2958eb81fd28d37ec7cf670b0b
Shai Mann-Robison <mann.robison@gmail.com>
2022-11-30 21:13:10 -0500

    Fix init.py issues

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -16,9 +16,9 @@ def create_app():
 
     # these are for the DB object to be able to connect to MySQL. 
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    app.config['MYSQL_DATABASE_PASSWORD'] = open('../../secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
-    app.config['MYSQL_DATABASE_PORT'] = 3306
+    app.config['MYSQL_DATABASE_PORT'] = 3200
     app.config['MYSQL_DATABASE_DB'] = 'GammaAir'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
