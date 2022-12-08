f76c46aa3912ef387ef9118a1f12951179698ff5
Shai Mann-Robison <mann.robison@gmail.com>
2022-12-07 20:04:13 -0500

    Update log_in.py

--- flask-app/src/log_in.py
+++ flask-app/src/log_in.py
@@ -16,14 +16,15 @@ current_user_id = -1
 
 # takes log in type as part of url (pilots/customers/representatives)
 # determines which table to look at for name + id
-@log_in.route('/log_in?userType=<userType>name=<name>&id=<userID>', methods=['GET'])
+@log_in.route('/log_in/<userType>?name=<name>&id=<userID>', methods=['GET'])
 def submit_log_in(userType, name, userID):
     query = 'select * from ' + tableMap.get(userType, "BAD_TABLE_NAME") \
             + ' where id == ' + userID + ' and lastName == ' + name
     json_data = execute_query(query)
 
     if len(json_data) > 0:
-        print(json_data[0])
+        current_user_type = userType
+        current_user_id = userID
         return True
 
     return False
