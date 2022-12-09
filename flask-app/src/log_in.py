a1542c771c49b7d5a968ccb11e407355198fcbd5
Shai Mann-Robison <mann.robison@gmail.com>
2022-12-09 17:55:25 -0500

    Update log_in.py

--- flask-app/src/log_in.py
+++ flask-app/src/log_in.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, current_app
-from src import db, execute_query
+from flask import Blueprint, request, current_app, jsonify
+from src import execute_query
 
 log_in = Blueprint('log_in', __name__)
 
@@ -10,28 +10,28 @@ tableMap = {
     "representatives": "CustomerRep",
 }
 
-current_user_type = False
-current_user_id = -1
+current_user_type = "Pilots"
+current_user_id = 4
 
 
 # takes log in type as part of url (pilots/customers/representatives)
 # determines which table to look at for name + id
-@log_in.route('/log_in', methods=['GET'])
+@log_in.route('/log-in', methods=['GET'])
 def submit_log_in():
     args = request.args.to_dict()
     query = 'select * from ' + tableMap.get(args['userType'], "BAD_TABLE_NAME") \
             + ' where id = ' + args['id'] + ' and lastName = "' + args['name'] + '"'
-    current_app.logger.info(query)
     json_data = execute_query(query)
 
     if len(json_data) > 0:
         current_user_type = tableMap.get(args['userType'], "BAD_TABLE_NAME")
-        current_user_id = args['userID']
+        current_user_id = args['id']
         return 'Success!'
 
     return False
 
 
-@log_in.route('/get_user_name', methods=['GET'])
+@log_in.route('/get-user-name', methods=['GET'])
 def get_user_name():
-    return "shai"
+    data = execute_query("select firstName from {0} where id = {1}".format(current_user_type, current_user_id))
+    return data[0].get("firstName")
