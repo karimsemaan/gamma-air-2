03473f8425be51495536d40cc6f7f76bdc4fb05f
Shai Mann-Robison <mann.robison@gmail.com>
2022-12-10 21:16:29 -0500

    go crazy go stupid

--- db/temp.sql
+++ db/temp.sql
@@ -1,30 +1,4 @@
-from flask import Blueprint
-from src import execute_query
-from src.log_in import current_user_id
-
-pilots = Blueprint('pilots', __name__)
-
-
-@pilots.route('/home', methods=['GET'])
-def home():
-    return '<h1>Pilots: Home</h1>'
-
-
-# SCHEDULES
-
-
-# Display's the pilot's current schedule
-@pilots.route('/schedule', methods=['GET'])
-def schedule():
-    # departure time/airport/gate
-    # arrival time/airport/gate
-    # plane type
-    # Pilot name
-    # Copilot name
-    # number of people on the plane
-
-    query = '''
-            select departureTime,
+select departureTime,
        departureGate,
        fromCode as fromAirport,
        arrivalTime,
@@ -58,27 +32,3 @@ from (select *
                join (select id as coPilotId, firstName as coPilotFirstName, lastName as coPilotLastName from Pilots) p
                     on p.coPilotId = f.coPilot) f
          join (select flight as ticketID, count(*) as numBookings from Tickets group by flight) t on f.id = t.ticketID
-            '''
-    data = execute_query(query)
-    return data
-
-
-# allows the pilot to submit a rescheduling request
-@pilots.route('/schedule/change-schedule/<flightID>', methods=['POST'])
-def schedule_change_flight(flightID):
-    return '<h1>Pilots: Submit schedule change request for flight #' + flightID + '</h1>'
-
-
-# REVIEWS
-
-
-# displays all the reviews of all the flights
-# this pilot has been responsible for
-@pilots.route('/my-reviews', methods=['GET'])
-def pilot_reviews():
-    query = ''' select * from 
-            (select * from CustomerRep where id = {}) natural join Reviews
-            '''
-    data = execute_query(query.format(current_user_id))
-    return '<h1>Pilots: My Reviews<h1>'
-
